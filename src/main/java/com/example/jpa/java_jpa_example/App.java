722d70f3428fadcb1d48e733f5e9e24ea32a830d
= <knoc720@gmail.com>
2017-04-28 11:28:26 +0900

    first commit

---
+++ src/main/java/com/example/jpa/java_jpa_example/App.java
@@ -0,0 +1,13 @@
+package com.example.jpa.java_jpa_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
